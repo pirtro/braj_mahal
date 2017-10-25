34a9de5e18f2e198aab6fded94a221e66141476f
Martin Haagensli <martin.haagensli@gmail.com>
2017-10-25 10:05:15 +0200

    contact form

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello eses!")
+document.addEventListener("DOMContentLoaded", function(event) {
+  const form = document.getElementById("contact");
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const { email, subject, message } = form.elements;
+    window.open(`mailto:${email.value}?subject=${subject.value}&body=${message.value}`);    
+  });
+});
